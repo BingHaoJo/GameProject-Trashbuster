625f095c6ca8a415ebff1b19002415619c5d0bf3
Chuan BIng Hao <xinghao6511@gmail.com>
2026-04-06 16:14:10 +1000

    Tilemaps sliced

--- Trashbuster/Assets/Scripts/CameraController.cs
+++ Trashbuster/Assets/Scripts/CameraController.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
+    [SerializeField] private int y_offset = 4;
 
     private void LateUpdate()
     {
         print(transform.position);
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + y_offset, transform.position.z);
     }
 }
