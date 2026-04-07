217eff912980aa25c9bafb372927bdf341ebe2d4
Chuan BIng Hao <xinghao6511@gmail.com>
2026-04-07 15:52:40 +1000

    Flipping character

--- Trashbuster/Assets/Scripts/PlayerController.cs
+++ Trashbuster/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private VacuumGunController vacuumGunController;
 
     private bool isGrounded;
     private Rigidbody2D rb;
     private InputAction moveAction;
     private InputAction jumpAction;
 
+    void OnEnable()
+    {
+        if (vacuumGunController != null)
+        {
+            vacuumGunController.mousePositionUpdated += OnMousePositionUpdated; // signal connected
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
         // Input actions
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
@@ -42,5 +51,27 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void OnMousePositionUpdated(Vector2 mousePos)
+    {
+        if (mousePos.x > transform.position.x + 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            // print("Facing Right" + mousePos.x + " /" + transform.position.x);
+        }
+        else if (mousePos.x < transform.position.x - 0.1f) // Adding a small threshold to prevent jitter when mouse is near the player's center
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            // print("Facing Left" + mousePos.x + " /" + transform.position.x);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (vacuumGunController != null)
+        {
+            vacuumGunController.mousePositionUpdated -= OnMousePositionUpdated; // signal disconnected
+        }
     }
 }
--- Trashbuster/Assets/Scripts/VacuumGunController.cs
+++ Trashbuster/Assets/Scripts/VacuumGunController.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class VacuumGunController : MonoBehaviour
 {
+    public event Action<Vector2> mousePositionUpdated;
     private Camera mainCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,13 +14,17 @@ public class VacuumGunController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {   
         Vector2 mousePos = Mouse.current.position.ReadValue();
+
         Vector3 worldPos = mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
         Vector2 direction = (worldPos - transform.position).normalized;
-        float angleRad = Mathf.Atan2(direction.y, direction.x);
-        float angleDeg = angleRad * Mathf.Rad2Deg; // Subtract 90 degrees to align with the sprite's orientation
+
+        mousePositionUpdated?.Invoke(direction); // signal emitted
+        // Debug.Log($"Mouse direction: {direction}");
+
+        float angleDeg = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
         Debug.DrawLine(transform.position, worldPos, Color.red, Time.deltaTime);
